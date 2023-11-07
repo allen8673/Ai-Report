f8e1e4279f18ccc034fae9d0a484a839266ab101
allen <allen8673@gmail.com>
2023-11-07 16:24:24 +0800

    implement the graph

---
+++ src/components/graph/graph.d.ts
@@ -0,0 +1,64 @@
+import { Node, Edge, ReactFlowInstance, ReactFlowProps, NodeChange, XYPosition } from 'reactflow';
+
+export type AddEdge = Edge | Connection;
+export type EventType = 'drop' | 'change' | '';
+export interface IGrapth<NData, EData = any, NNormal = NData, ENormal = EData> extends Omit<ReactFlowProps,
+    | 'nodes'
+    | 'edges'
+    | 'defaultNodes'
+    | 'defaultEdges'
+    | 'minZoom'
+    | 'onLoad'
+    | 'onInit'
+    | 'onEdgeUpdate'
+    | 'onNodesChange'
+    | 'onEdgesChange'
+    | 'onSelectionChange'
+    | 'onDragOver'
+    | 'onDrop'
+    | 'onMouseUp'
+    | 'onMouseDown'
+    | 'onContextMenu'
+> {
+    className?: string
+    initialNodes?: Node<NData>[];
+    initialEdges?: Edge<EData>[];
+    readonly?: boolean;
+    hideMiniMap?: boolean;
+    graphRef?: React.Ref<GraphInstance<NData, EData>>;
+    edgeEditable?: boolean;
+    onLoad?: (rfi?: ReactFlowInstance) => void;
+    normalizeNode?: (node: Node<NData>) => Node<NNormal>;
+    normalizeEdge?: (edge: Edge<EData>) => Edge<ENormal>;
+    onNodesSelected?: (nodes: Node<NNormal>[]) => void;
+    onEdgesSelected?: (edges: Edge<ENormal>[]) => void;
+    onNodesChange?: (changes: NodeChange[], result: Node<NData>[]) => void;
+    onEdgesChange?: (result: Edge<EData>[]) => void;
+    onDrop?: (type: string, position: XYPosition | undefined, payload?: any) => void;
+    onMouseUp?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>, position: XYPosition | undefined) => void;
+    onMouseDown?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>, position: XYPosition | undefined) => void;
+    onReadonly?: (eventType: EventType) => void;
+    onDiagramsContextMenu?: (event: React.MouseEvent<Element, MouseEvent>, elem?: Node<NNormal> | Edge<ENormal>) => void;
+}
+
+/**
+ * Interface of tree instance that include the exposed funcionalities
+ * @param addNode add node
+ * @param removeNode remove node by ids
+ * @param addEdge add edge
+ * @param removeNode remove edge by id
+ * @param resetAllElements reset all elements include all nodes and edges
+ */
+export interface GraphInstance<NData, EData = any> {
+    addNode: (node: Node<NData>) => void;
+    setNode: (nodeId: string, data: ((oldNode: Node<NData>) => Node<NData>) | Node<NData>) => void;
+    setNodes: (data: ((oldNode: Node<NData>) => Node<NData>) | Node<NData>) => void;
+    removeNode: (nodeId: string) => void;
+    duplicateNode: (nodeId: string, setNewNode?: (node: Node<NData>) => Node<NData>) => void;
+    addEdge: (edge: Edge<EData>) => void;
+    setEdge: (edgeId: string, data: ((oldEdge: Edge<EData>) => Edge<EData>) | Edge<EData>) => void;
+    setEdges: (data: ((oldEdge: Edge<EData>) => Edge<EData>) | Edge<EData>) => void;
+    removeEdge: (edgeId: string) => void;
+    resetAllElements: (nodes: Node<NData>[], edges: Edge<EData>[]) => void;
+    reactFlowInstance?: ReactFlowInstance;
+}
--- src/components/graph/index.tsx
+++ src/components/graph/index.tsx
@@ -1,54 +1,367 @@
 'use client'
 
-import { useCallback, useRef } from "react";
+import _ from "lodash";
+import { useCallback, useEffect, useImperativeHandle, useMemo, useRef, useState } from "react";
 import ReactFlow, {
     Node,
     Edge,
-    useNodesState,
-    useEdgesState,
-    addEdge,
     Connection,
-    ConnectionLineType,
     MiniMap,
     Controls,
-    Background
+    Background,
+    NodeChange,
+    applyNodeChanges,
+    ReactFlowInstance,
+    EdgeChange,
+    applyEdgeChanges,
 } from 'reactflow';
 import 'reactflow/dist/style.css';
+import { v4 } from "uuid";
 
-export interface IGrapth<NData, EData> {
-    className?: string
-    initialNodes?: Node<NData>[];
-    initialEdges?: Edge<EData>[];
-    hideMiniMap?: boolean;
+import { EventType, GraphInstance, IGrapth } from "./graph";
 
-}
 
-export default function Grapth<NData, EData>(props: IGrapth<NData, EData>) {
 
-    const { className, initialNodes, initialEdges, hideMiniMap } = props;
+export default function Grapth<NData, EData, NNormal, ENormal>(props: IGrapth<NData, EData, NNormal, ENormal>) {
+
+    const {
+        className,
+        initialNodes,
+        initialEdges,
+        readonly,
+        hideMiniMap,
+        graphRef,
+        edgeEditable,
+        normalizeNode,
+        normalizeEdge,
+        onLoad,
+        onNodesSelected,
+        onEdgesSelected,
+        onNodesChange,
+        onEdgesChange,
+        onDrop,
+        onMouseUp,
+        onMouseDown,
+        onNodeMouseMove,
+        onReadonly,
+        onDiagramsContextMenu,
+        onNodeContextMenu,
+        onEdgeContextMenu,
+        ...others } = props;
 
     const rfWrapper = useRef<HTMLDivElement>(null);
-    const [nodes, setNodes, onNodesChange] = useNodesState<NData>(initialNodes || []);
-    const [edges, setEdges, onEdgesChange] = useEdgesState<EData>(initialEdges || []);
+    const [reactFlowInstance, setReactFlowInstance] = useState<ReactFlowInstance>();
+    const [ondrag, setOndrag] = useState<boolean>();
+    const [nodes, setNodes] = useState<Node<NData>[]>([]);
+    const [edges, setEdges] = useState<Edge<EData>[]>([]);
 
-    const onConnect = useCallback(
-        (params: Connection | Edge) => setEdges((eds) => addEdge(params, eds)),
-        [setEdges]
+    useEffect(() => {
+        setNodes(initialNodes || []);
+        setEdges(initialEdges || []);
+    }, []);
+
+    useEffect(() => {
+        _setNodes(
+            _.map<Node<NData>, NodeChange>(nodes, (n) => ({
+                type: 'reset',
+                item: { ...n, draggable: n.draggable && !readonly },
+            })),
+            ''
+        );
+    }, [readonly]);
+
+    /**
+     * Announce exposed functionalities by treeRef
+     */
+    useImperativeHandle<GraphInstance<NData, EData>, GraphInstance<NData, EData>>(graphRef, () => ({
+        addNode: (node: Node<NData>): void => {
+            _setNodes([{ type: 'add', item: node }], 'change');
+        },
+        setNode: (nodeId: string, data: ((oldNode: Node<NData>) => Node<NData>) | Node<NData>): void => {
+            _setNodes(
+                _.map<Node<NData>, NodeChange>(nodes, (n) => {
+                    if (n.id === nodeId) {
+                        if (typeof data === 'function') {
+                            return { type: 'reset', item: data(_.cloneDeep(n)) };
+                        } else {
+                            return { type: 'reset', item: data };
+                        }
+                    }
+                    return {
+                        type: 'reset',
+                        item: n,
+                    };
+                }),
+                'change'
+            );
+        },
+        setNodes: (data: ((oldNode: Node<NData>) => Node<NData>) | Node<NData>): void => {
+            _setNodes(
+                _.map<Node<NData>, NodeChange>(nodes, (n) => {
+                    if (typeof data === 'function') {
+                        return { type: 'reset', item: data(_.cloneDeep(n)) };
+                    } else {
+                        return { type: 'reset', item: data };
+                    }
+                }),
+                'change'
+            );
+        },
+        removeNode: (nodeId: string): void => {
+            const connectEdge = _.filter(edges, (e) => e.source === nodeId || e.target === nodeId);
+            _setNodes([{ type: 'remove', id: nodeId }], 'change');
+            _setEdges(_.map(connectEdge, (e) => ({ type: 'remove', id: e.id })));
+        },
+        duplicateNode: (nodeId: string, setNewNode?: (node: Node<NData>) => Node<NData>): void => {
+            const node = _.find(nodes, ['id', nodeId]);
+            if (!node) return;
+
+            const newNodeId = v4();
+            const x = (node as Node<NData>)?.position?.x || 0;
+            const y = (node as Node<NData>)?.position?.y || 0;
+
+            const newNode: Node<NData> = {
+                ...node,
+                id: newNodeId,
+                position: {
+                    x: x + 50,
+                    y: y + 50,
+                },
+            };
+
+            _setNodes([{ type: 'add', item: !!setNewNode ? setNewNode(newNode) : newNode }], 'change');
+        },
+        addEdge: (edge: Edge<EData>): void => {
+            _setEdges([{ type: 'add', item: edge }]);
+        },
+        setEdge: (edgeId: string, data: ((oldNode: Edge<EData>) => Edge<EData>) | Edge<EData>): void => {
+            _setEdges(
+                _.map<Edge<EData>, EdgeChange>(edges, (e) => {
+                    if (e.id === edgeId) {
+                        if (typeof data === 'function') {
+                            return { type: 'reset', item: data(_.cloneDeep(e)) };
+                        } else {
+                            return { type: 'reset', item: data };
+                        }
+                    }
+                    return {
+                        type: 'reset',
+                        item: e,
+                    };
+                })
+            );
+        },
+        setEdges: (data: ((oldNode: Edge<EData>) => Edge<EData>) | Edge<EData>): void => {
+            _setEdges(
+                _.map<Edge<EData>, EdgeChange>(edges, (e) => {
+                    if (typeof data === 'function') {
+                        return { type: 'reset', item: data(_.cloneDeep(e)) };
+                    } else {
+                        return { type: 'reset', item: data };
+                    }
+                })
+            );
+        },
+        removeEdge: (edgeId: string): void => {
+            _setEdges([{ type: 'remove', id: edgeId }]);
+        },
+        resetAllElements: (_nodes: Node<NData>[], _edges: Edge<EData>[]): void => {
+            setNodes(() => _nodes);
+            setEdges(() => _edges);
+        },
+        reactFlowInstance,
+    }));
+
+    const _onInit = useCallback(
+        (rfi: any) => {
+            if (!reactFlowInstance) {
+                setReactFlowInstance(rfi);
+            }
+            onLoad?.(rfi);
+        },
+        [reactFlowInstance]
     );
+
+    const debounceOnNodesChange = useMemo(() => {
+        return _.debounce((changes: NodeChange[], newNodes: Node<NData>[]) => {
+            onNodesChange?.(changes, newNodes);
+        }, 300);
+    }, [onNodesChange]);
+
+    const debounceonEdgesChange = useMemo(() => {
+        return _.debounce((newEdges: Edge<EData>[]) => {
+            onEdgesChange?.(newEdges);
+        }, 300);
+    }, [onEdgesChange]);
+
+    const _setNodes = useMemo(
+        () =>
+            (changes: NodeChange[], eventType: EventType): void => {
+                if (!!readonly) {
+                    if (!!eventType) onReadonly?.(eventType);
+                    return;
+                }
+                setNodes((ns) => {
+                    const newNodes = applyNodeChanges(changes, ns);
+
+                    if (_.isEqual(ns, newNodes)) {
+                        return ns;
+                    } else {
+                        debounceOnNodesChange(changes, newNodes);
+                        // onNodesChange?.(changes, newNodes);
+                        return newNodes;
+                    }
+                });
+            },
+        []
+    );
+
+    const _setEdges = (changes: EdgeChange[], edges?: Edge<EData>[]): void => {
+        setEdges((es) => {
+            const newEdges = !_.isEmpty(changes) ? applyEdgeChanges(changes, es) : edges || es;
+            if (_.isEqual(es, newEdges)) return es;
+            debounceonEdgesChange(newEdges);
+            // onEdgesChange?.(newEdges);
+            return newEdges;
+        });
+    };
+
+    const _onSelectionChange = useCallback(({ nodes: _n, edges: _e }: { nodes: Node<NNormal>[]; edges: Edge<ENormal>[] }): void => {
+        onNodesSelected?.(_n);
+        onEdgesSelected?.(_e);
+    }, []);
+
+    const _onEdgeUpdate = useCallback((oldEdge: Edge<EData>, newConnection: Connection) => {
+        const { source, target, ...handles } = newConnection;
+        if (!source || !target) return;
+
+        _setEdges(
+            _.map<Edge<EData>, EdgeChange>(edges, (e) => {
+                if (e.id === oldEdge.id) {
+                    return {
+                        type: 'reset',
+                        item: { ...oldEdge, ...handles, source, target },
+                    };
+                }
+                return {
+                    type: 'reset',
+                    item: e,
+                };
+            })
+        );
+
+        // _setEdges([
+        //     {
+        //         type: 'reset',
+        //         item: { ...oldEdge, ...handles, source, target },
+        //     },
+        // ]);
+    }, []);
+
+    const _onNodesChange = useCallback((changes: NodeChange[]): void => {
+        _setNodes(changes, _.some(changes, (c: NodeChange) => _.includes(['add', 'remove', 'reset'], c.type)) ? 'change' : '');
+    }, []);
+
+    const _onEdgesChange = useCallback((changes: EdgeChange[]) => {
+        _setEdges(changes);
+    }, []);
+
+    const _onElementsDragOver = (event: React.DragEvent<HTMLDivElement>): void => {
+        event.stopPropagation();
+        event.preventDefault();
+        event.dataTransfer.dropEffect = 'move';
+    };
+
+    const _onMouseUp = (event: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
+        if (readonly) {
+            setOndrag((pre) => false);
+        }
+
+        if (!rfWrapper?.current || !onMouseUp) return;
+        const reactFlowBounds = rfWrapper?.current.getBoundingClientRect();
+
+        const position = reactFlowInstance?.project({
+            x: event.clientX - reactFlowBounds.left,
+            y: event.clientY - reactFlowBounds.top,
+        });
+
+        onMouseUp(event, position);
+    };
+
+    const _onMouseDown = (event: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
+        if (readonly) {
+            setOndrag((pre) => true);
+        }
+
+        if (!rfWrapper?.current || !onMouseDown) return;
+        const reactFlowBounds = rfWrapper?.current.getBoundingClientRect();
+
+        const position = reactFlowInstance?.project({
+            x: event.clientX - reactFlowBounds.left,
+            y: event.clientY - reactFlowBounds.top,
+        });
+
+        onMouseDown(event, position);
+    };
+
+    const _onElementsDrop = (event: React.DragEvent<HTMLDivElement>): void => {
+        event.stopPropagation();
+        event.preventDefault();
+
+        if (!rfWrapper?.current || !onDrop) return;
+        const reactFlowBounds = rfWrapper?.current.getBoundingClientRect();
+        const type = event.dataTransfer.getData('application/type');
+        let payload = event.dataTransfer.getData('application/payload');
+        try {
+            payload = JSON.parse(payload);
+        } catch (e) {
+            //
+        }
+        const position = reactFlowInstance?.project({
+            x: event.clientX - reactFlowBounds.left,
+            y: event.clientY - reactFlowBounds.top,
+        });
+
+        onDrop(type, position, payload);
+    };
+
+    const _onNodeMouseMove = (event: React.MouseEvent, node: Node<NData>): void => {
+        onNodeMouseMove?.(event, node);
+        const viewport = reactFlowInstance?.toObject().viewport;
+        if (!ondrag || !viewport) return;
+        reactFlowInstance?.setViewport({
+            zoom: viewport.zoom,
+            x: viewport.x + event.movementX,
+            y: viewport.y + event.movementY,
+        });
+    };
 
     return <div id='graph' className={`w-full h-full std-border std-content ${className}`} ref={rfWrapper}>
         <ReactFlow
-            nodes={nodes}
-            edges={edges}
-            onNodesChange={onNodesChange}
-            onEdgesChange={onEdgesChange}
-            onConnect={onConnect}
-            defaultEdgeOptions={{
-                animated: true,
-                type: 'smoothstep',
+            nodes={!!normalizeNode ? _.map(nodes, (n) => normalizeNode(n)) : nodes}
+            edges={!!normalizeEdge ? _.map(edges, (e) => normalizeEdge(e)) : edges}
+            minZoom={0.1}
+            onInit={_onInit}
+            onEdgeUpdate={!readonly && edgeEditable ? _onEdgeUpdate : undefined}
+            onNodesChange={_onNodesChange}
+            onEdgesChange={_onEdgesChange}
+            onSelectionChange={_onSelectionChange}
+            onDragOver={_onElementsDragOver}
+            onDrop={_onElementsDrop}
+            onMouseDown={_onMouseDown}
+            onMouseUp={_onMouseUp}
+            snapToGrid={true}
+            snapGrid={[10, 10]}
+            onNodeContextMenu={(e, node): void => {
+                onDiagramsContextMenu?.(e, node as Node<NNormal>);
+                onNodeContextMenu?.(e, node);
             }}
-            connectionLineType={ConnectionLineType.SmoothStep}
-            fitView
+            onEdgeContextMenu={(e, edge): void => {
+                onDiagramsContextMenu?.(e, edge as Edge<ENormal>);
+                onEdgeContextMenu?.(e, edge);
+            }}
+            onNodeMouseMove={_onNodeMouseMove}
+            {...others}
         >
             {!hideMiniMap && <MiniMap nodeBorderRadius={2} />}
             <Controls />
