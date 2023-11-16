4e6c06084df4bbdc7d8abdcf1eedb851a615bee9
allen <allen8673@gmail.com>
2023-11-16 10:56:55 +0800

    remove the redundant container

--- src/app/template/page.tsx
+++ src/app/template/page.tsx
@@ -1,8 +1,46 @@
-import TemplateOverview from "@/containers/template-overview";
+'use client'
+import { faAdd } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button } from 'primereact/button';
+
+import Table from '@/components/table';
+import { Column } from '@/components/table/table';
+import TitlePane from '@/components/title-pane';
+import { ITemplate } from '@/interface/workflow';
 import { mock_template } from "@/mock-data/mock";
 
 export default function Page() {
-    return (
-        <TemplateOverview templates={mock_template} />
-    );
+
+    const templates = mock_template;
+
+    const columns: Column<ITemplate>[] = [
+        { key: 'id', title: 'ID' },
+        { key: 'name', title: 'Name' }
+    ]
+
+    return <div className="flex h-full flex-col gap-std items-stretch text-light">
+        <TitlePane
+            title='Template List'
+            postContent={
+                <>
+                    <Button icon={<FontAwesomeIcon className='mr-[7px]' icon={faAdd} />}
+                        severity="success"
+                        label='Add New Template'
+                        tooltipOptions={{ position: 'left' }}
+                        onClick={() => {
+
+                        }}
+                    />
+                </>}
+        />
+        <Table className='' data={templates} columns={columns}
+            paginator rows={10}
+            first={0}
+            totalRecords={5}
+            onRowClick={() => {
+                // 
+            }}
+        />
+    </div>
+
 }
