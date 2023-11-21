55730e3f86a412ddc1a9d99b3f1cadbe85c74a3a
allen.lin <allen8673@gmail.com>
2023-11-21 10:47:14 +0800

    enhance the form component

--- src/components/form/form.d.ts
+++ src/components/form/form.d.ts
@@ -1,14 +1,28 @@
 
 import React from "react";
-import { ControllerFieldState, ControllerRenderProps, Path, RegisterOptions, UseFormReturn, UseFormStateReturn } from "react-hook-form";
+import {
+    ControllerFieldState,
+    ControllerRenderProps,
+    Path,
+    PathValue,
+    RegisterOptions,
+    UseFormReturn,
+    UseFormStateReturn
+} from "react-hook-form";
 
 
 
 export type FormValue = Record<string, any>;
-export type FormInstance<T extends FormValue> = UseFormReturn<T, T, any>
-
+export type FormCoreInstance<T extends FormValue> = UseFormReturn<T, T, any>
+export interface FormInstance<T extends FormValue> {
+    getValues: () => T;
+    setValue: (name: Path<T>, value: PathValue<T, Path<T>>, options?: Partial<{ shouldValidate: boolean; shouldDirty: boolean; shouldTouch: boolean; }>) => void
+    formCore: FormCoreInstance<T>;
+    submit: () => Promise<T>
+}
 export interface GetItemProps<T extends FormValue> {
-    formInstance: FormInstance<T>
+    formCore: FormCoreInstance<T>;
+    className?: string;
 }
 
 export interface FormItemChildren<T extends FormValue> {
@@ -22,5 +36,5 @@ export interface FormItemProps<T extends FormValue> {
     children: React.ReactElement | ((props: FormItemChildren<T>) => React.ReactElement);
     label?: string;
     rules?: Omit<RegisterOptions<T, Path<T>>, "disabled" | "valueAsNumber" | "valueAsDate" | "setValueAs">;
-    valuePropName?: string
+    valuePropName?: string;
 }
