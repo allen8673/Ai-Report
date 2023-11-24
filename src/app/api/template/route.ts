b8b1d6e146a704f5fc7fc7bff6702b9251ea178b
allen <allen8673@gmail.com>
2023-11-25 07:24:37 +0800

    build test

--- src/app/api/template/route.ts
+++ src/app/api/template/route.ts
@@ -1,17 +1,12 @@
 import _ from "lodash"
-import { useSearchParams } from "next/navigation";
 import { NextRequest } from 'next/server'
 
 import { mock_templates } from "@/mock-data/mock"
 
-// export const dynamic = "force-static";
 export async function GET(request: NextRequest) {
-    const searchParams = useSearchParams();
-    // const searchParams = request.nextUrl.searchParams
-    const id = searchParams.get('id')
-
-    console.log(id)
-    const data = _.map(mock_templates, d => ({ id: d.id, name: d.name }))
+    // const searchParams = useSearchParams();
+    const id = request.nextUrl.searchParams.get('id');
+    const data = id ? _.find(mock_templates, ['id', id]) : _.map(mock_templates, d => ({ id: d.id, name: d.name }))
     return Response.json({ data })
 }
 
