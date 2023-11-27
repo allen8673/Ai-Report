5bf641b39b65fc5270c30ef185d36f558b95a7b4
allen.lin <allen8673@gmail.com>
2023-11-27 08:45:29 +0800

    close the dynamic frontend server

--- src/app/api/template/route.ts
+++ src/app/api/template/route.ts
@@ -1,13 +1,14 @@
-import _ from "lodash"
-import { NextRequest } from 'next/server'
+// import _ from "lodash"
+// import { NextRequest } from 'next/server'
 
-import { mock_templates } from "@/mock-data/mock"
+// import { mock_templates } from "@/mock-data/mock"
 
-export async function GET(request: NextRequest) {
-    // const searchParams = useSearchParams();
-    const id = request.nextUrl.searchParams.get('id');
-    const data = id ? _.find(mock_templates, ['id', id]) : _.map(mock_templates, d => ({ id: d.id, name: d.name }))
-    return Response.json({ data })
+// export async function GET(request: NextRequest) {
+//     const id = request.nextUrl.searchParams.get('id');
+//     const data = id ? _.find(mock_templates, ['id', id]) : _.map(mock_templates, d => ({ id: d.id, name: d.name }))
+//     return Response.json({ data })
+// }
+
+export async function GET() {
+    return Response.json({ data: { hello: 'hello' } })
 }
-
-
