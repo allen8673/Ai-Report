bf78bc24aeb33ee644b805c2e6dbb500af3f0cf4
allen.lin <allen8673@gmail.com>
2023-12-11 09:46:18 +0800

    modify the upload modal when runing the workflow

--- src/app/workflow/workflow-editor/page.tsx
+++ src/app/workflow/workflow-editor/page.tsx
@@ -13,6 +13,7 @@ import { v4 } from "uuid";
 
 import apiCaller from "@/api-helpers/api-caller";
 import { coverSearchParamsToObj } from "@/api-helpers/url-helper";
+import FileUploader from "@/components/file-uploader";
 import FlowGraph from "@/components/flow-editor";
 import { flowInfoMap } from "@/components/flow-editor/configuration";
 import { IWorkflowMap } from "@/components/flow-editor/context";
@@ -492,114 +493,23 @@ export default function Page() {
             <Modal
                 title="Upload your files"
                 visible={openUpload}
+                onOk={() => setOpenUpload(false)}
+                footerClass="flex justify-end"
             >
-                <Tooltip target=".custom-choose-btn" content="Add files" position="bottom" />
-                <Tooltip target=".custom-upload-btn" content="Upload and Run workflow" position="bottom" />
-                <Tooltip target=".custom-cancel-btn" content="Clear files" position="bottom" />
-
-                <FileUpload name="upload" url={''}
-                    mode='advanced'
-                    multiple
-                    accept="*"
-                    // maxFileSize={1000000}
-                    contentClassName="rounded-sm mt-1"
-                    headerTemplate={(opts) => {
-                        return <div
-                            className={`
-                            h-[60px] px-[12px]
-                            rounded-std-sm 
-                            border-solid
-                            border-light-weak
-                            flex items-center justify-between
-                            `}>
-                            <span className="flex gap-1">
-                                {opts.chooseButton}
-                                {opts.cancelButton}
-                            </span>
-                            <span className="flex gap-1">
-                                {opts.uploadButton}
-                                <Button
-                                    icon={<FontAwesomeIcon
-                                        className="w-[18px] h-[18px] p-[3px]"
-                                        icon={faXmark}
-                                    />}
-                                    className="p-button-rounded p-button-outlined border-2"
-                                    style={{ color: 'rgba(185, 28, 28, 1)' }}
-                                    tooltip="Cancel"
-                                    onClick={() => {
-                                        setOpenUpload(false)
-                                    }}
-                                />
-
-                            </span>
-                        </div>
-                    }}
-                    chooseOptions={{
-                        icon: <FontAwesomeIcon
-                            className="w-[18px] h-[18px]  p-[3px]"
-                            icon={faAdd}
-                        />,
-                        iconOnly: true,
-                        className: 'custom-choose-btn p-button-rounded p-button-outlined border-2',
-                        style: { color: '#BA4AFF' }
-                    }}
-                    uploadOptions={{
-                        icon: <FontAwesomeIcon
-                            className="w-[18px] h-[18px] p-[3px]"
-                            icon={faCloudUpload}
-                        />,
-                        iconOnly: true,
-                        className: 'custom-upload-btn p-button-rounded p-button-outlined border-2',
-                        style: { color: '#2a8af6' }
-                    }}
-                    cancelOptions={{
-                        icon: <FontAwesomeIcon
-                            className="w-[18px] h-[18px] p-[3px]"
-                            icon={faEraser}
-                        />,
-                        iconOnly: true,
-                        className: 'custom-cancel-btn p-button-rounded p-button-outlined border-2',
-                        style: { color: 'rgba(185, 28, 28, 1)' }
-                    }}
-                    emptyTemplate={<p className="m-0">Drag and drop files to here to upload.</p>}
-                    itemTemplate={(file: any, opts) => {
-                        const _file = (file as File);
-                        return (
-                            <div className="flex items-center gap-[7px] h-[45px] my-1">
-                                <span className="h-[40px] w-[40px] text-center">{
-                                    !!file?.objectURL ?
-                                        <img alt='' src={file?.objectURL} className="w-full h-full" /> :
-                                        <FontAwesomeIcon className="w-full h-full" icon={faFile} />
-                                }</span>
-                                <div className="flex grow items-center [&>div]:text-left">
-                                    <div className="flex flex-column grow">
-                                        <div>{_file.name}</div>
-                                        <div>{_file.size}</div>
-                                    </div>
-                                    <FontAwesomeIcon
-                                        className=''
-                                        icon={faTrash}
-                                        onClick={(e) => { opts.onRemove(e) }}
-                                    />
-                                </div>
-                            </div>
-                        )
-                    }}
-                    customUpload
-                    uploadHandler={e => {
-
+                <FileUploader
+                    uploadLabel="Upload & Run"
+                    onUpload={e => {
                         if (workflow && e.files && e.files.length > 0) {
                             const formData = new FormData();
-                            for (let i = 0; i < e.files.length; i++) {
-                                const file = e.files[i]
-                                formData.append('files', file);
+                            for (const i in e.files) {
+                                formData.append('files', e.files[i])
                             }
 
                             formData.append('userId', '23224');
                             formData.append('workflowId', workflow.id);
                             formData.append('version', '1');
 
-                            apiCaller.post<ApiResult>(`${process.env.NEXT_PUBLIC_RUN}`, formData, {
+                            apiCaller.post<ApiResult>(`${process.env.NEXT_PUBLIC_REPORT}/run`, formData, {
                                 headers: {
                                     "Content-Type": "multipart/form-data",
                                     // "x-rapidapi-host": "file-upload8.p.rapidapi.com",
@@ -607,7 +517,6 @@ export default function Page() {
                                 },
                             });
                         }
-
                     }}
                 />
             </Modal>
