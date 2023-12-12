9ad05afbc23acaf6c33a0cb4d51f1a866dbf96e8
allen.lin <allen8673@gmail.com>
2023-12-12 10:00:04 +0800

    add empty pane

---
+++ src/components/empty-pane/index.tsx
@@ -0,0 +1,18 @@
+import { faInbox } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+interface EmptyPaneProps {
+    title?: string
+}
+export default function EmptyPane({ title }: EmptyPaneProps) {
+    return (
+        <div className={`
+        bg-deep-weak
+        text-light text-xl 
+        w-full h-full 
+        flex flex-col grow justify-center items-center 
+        border-solid border-light-weak rounded-std-sm `}>
+            <FontAwesomeIcon className='w-16 h-16' icon={faInbox} />
+            <p className='my-0'>{title || 'No Data'}</p>
+        </div>)
+}
