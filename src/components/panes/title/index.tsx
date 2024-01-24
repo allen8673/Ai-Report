0845e2bf03e258c7f7cd86de74ca92c98fe7e07b
allen.lin <allen8673@gmail.com>
2024-01-24 15:48:49 +0800

    adapt the style of title pane

--- src/components/panes/title/index.tsx
+++ src/components/panes/title/index.tsx
@@ -5,8 +5,11 @@ export interface TitlePaneProps {
     postContent?: React.ReactNode
 }
 export default function TitlePane({ title, postContent }: TitlePaneProps) {
-    return <div className="rounded-std h-[60px] py-[18px] flex items-center justify-between text-light text-[22px] ">
-        {title}
+    return <div className={`
+            h-[60px] p-5 rounded-std m-1
+            bg-deep-weak/[.4] border-light-weak/[.2] border-solid
+            flex items-center justify-between`}>
+        <h1 className="text-[32px]">{title}</h1>
         {!!postContent &&
             <div className="flex gap-std-sm">
                 {postContent}
