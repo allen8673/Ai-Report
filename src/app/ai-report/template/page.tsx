d3f38ba3dff38b87f8d38826c3f2501ccb673a96
allen.lin <allen8673@gmail.com>
2024-01-25 11:17:49 +0800

    modify the layout of template page

--- src/app/ai-report/template/page.tsx
+++ src/app/ai-report/template/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
-import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { map } from 'lodash';
 import { Button } from 'primereact/button';
 import { confirmDialog } from 'primereact/confirmdialog';
@@ -10,152 +8,13 @@ import { useEffect, useState } from 'react';
 
 import { deleteFlow, getFlow, getFlows, updateFlow } from '@/api-helpers/flow-api';
 import FlowEditor from '@/components/flow-editor';
+import FlowList from '@/components/flow-list';
 import { useGraphRef } from '@/components/graph';
 import Modal from '@/components/modal';
 import EmptyPane from '@/components/panes/empty';
 import TitlePane from '@/components/panes/title';
-import Table from '@/components/table';
-import { Column } from '@/components/table/table';
 import { IFlowNode, IFlow, IFlowBase } from '@/interface/flow';
 import { useLayoutContext } from '@/layout/standard-layout/context';
-
-export default function Page() {
-
-    const [templates, setTemplates] = useState<IFlowBase[]>([]);
-    const [selection, setSelection] = useState<IFlow>();
-    const [editTemp, setEditTemp] = useState<IFlow>();
-    const { showMessage } = useLayoutContext();
-
-    const { graphRef } = useGraphRef<IFlowNode, any>();
-    const columns: Column<IFlowBase>[] = [
-        { key: 'id', title: 'ID', style: { width: '25%' } },
-        { key: 'name', title: 'Name' },
-        {
-            style: { width: 80, padding: 0 },
-            format: (row) => {
-                return <div
-                    className="flex gap-[7px] px-[12px]"
-                    role='presentation'
-                    onClick={(e) => e.stopPropagation()}>
-                    <Button
-                        className="py-0 px-[0px] h-[40px]"
-                        severity='info'
-                        tooltip="Edit template"
-                        tooltipOptions={{ position: 'left' }}
-                        icon={<FontAwesomeIcon icon={faPen} />}
-                        onClick={async () => {
-                            const tmp = await getFlow(row.id)
-                            setEditTemp(tmp)
-                        }}
-                    />
-                    <Button
-                        className="py-0 px-[0px] h-[40px]"
-                        severity='danger'
-                        tooltip="Remove template"
-                        tooltipOptions={{ position: 'left' }}
-                        icon={
-                            <FontAwesomeIcon icon={faTrash} />
-                        }
-                        onClick={() => {
-                            confirmDialog({
-                                position: 'top',
-                                message: `Do you want to remove ${row?.name || 'this template'}?`,
-                                header: `Remove Template`,
-                                icon: 'pi pi-info-circle',
-                                acceptClassName: 'p-button-danger',
-                                accept: async () => {
-                                    const rsp = await deleteFlow(row?.id);
-                                    if (rsp.data.status === 'failure') return;
-                                    await fetchTemplates();
-                                    setSelection(pre => pre?.id === row.id ? undefined : pre)
-                                },
-                            });
-                        }}
-                    />
-
-                </div >
-            },
-
-        }
-    ];
-
-    const fetchTemplates = async () => {
-        const tmps = await getFlows('TEMPLATE');
-        setTemplates(tmps)
-    }
-
-    useEffect(() => {
-        fetchTemplates()
-    }, [])
-
-    return <div className="page-std">
-        <TitlePane title='Template' />
-        <Splitter className='shrink grow' style={{ height: '300px' }} layout="vertical">
-            <SplitterPanel className="px-[7px] " size={60}>
-                {selection?.flows ? <FlowEditor
-                    flows={selection.flows}
-                    graphRef={graphRef}
-                    hideMiniMap
-                    hideCtrls
-                    fitView
-                    fitViewOptions={{ duration: 1000 }}
-                    onNodesChange={(changes) => {
-                        if (changes.length > 1) {
-                            graphRef.current.reactFlowInstance?.fitView({ duration: 500 })
-                        }
-                    }}
-                /> : <EmptyPane icon={faPaperPlane} title='Select a template to show the graph' />}
-            </SplitterPanel>
-            <SplitterPanel className="overflow-auto px-[7px]" size={40}>
-                <Table className='h-full w-full'
-                    data={templates}
-                    columns={columns}
-                    paginator
-                    rows={10}
-                    first={0}
-                    totalRecords={5}
-                    onSelectionChange={async e => {
-                        const tmp = await getFlow(e.value.id)
-                        setSelection(tmp);
-                    }}
-                    selection={selection}
-                />
-            </SplitterPanel>
-        </Splitter>
-        <EditTemplateModal
-            editTemp={editTemp}
-            onOk={async (result) => {
-                const res = await updateFlow(result);
-                if (res.data.status === 'ok') {
-                    showMessage({
-                        type: 'success',
-                        message: res.data.message || 'Success',
-                    });
-                    setSelection(pre => result.id === pre?.id ? result : pre)
-                    setEditTemp(undefined);
-                } else {
-                    showMessage({
-                        type: 'error',
-                        message: res.data.message || 'error',
-                    });
-                }
-            }}
-            onCancel={() => {
-                confirmDialog({
-                    position: 'top',
-                    message: `Are you sure you want to cancel without saving? You will lose every modification.`,
-                    header: `Cancel modify`,
-                    icon: 'pi pi-info-circle',
-                    acceptClassName: 'p-button-danger',
-                    accept: async () => {
-                        setEditTemp(undefined)
-                    },
-                });
-            }}
-        />
-    </div>
-
-}
 
 interface EditTemplateModalProps {
     editTemp?: IFlow;
@@ -201,3 +60,147 @@ function EditTemplateModal({ editTemp, onOk, onCancel }: EditTemplateModalProps)
         />
     </Modal>
 }
+
+interface TemplatePreviewerProps {
+    template?: IFlow;
+    onClickEdit?: (tmp: IFlow) => void;
+    onDelete?: (tmp: IFlow) => void
+}
+
+function TemplatePreviewer({ template, onClickEdit, onDelete }: TemplatePreviewerProps) {
+    const { graphRef } = useGraphRef<IFlowNode, any>();
+
+    return template?.flows ?
+        <FlowEditor
+            flows={template.flows}
+            graphRef={graphRef}
+            hideMiniMap
+            hideCtrls
+            fitView
+            fitViewOptions={{ duration: 1000 }}
+            onNodesChange={(changes) => {
+                if (changes.length > 1) {
+                    graphRef.current.reactFlowInstance?.fitView({ duration: 500 })
+                }
+            }}
+            actionBarContent={
+                <div
+                    className="flex-h-center gap-[7px] justify-end w-full"
+                    role='presentation'
+                    onClick={(e) => e.stopPropagation()}>
+                    <Button
+                        className="py-0 px-[0px] h-[40px]"
+                        icon='pi pi-trash'
+                        severity='danger'
+                        tooltip="Remove Template"
+                        tooltipOptions={{ position: 'left' }}
+                        onClick={() => {
+                            confirmDialog({
+                                position: 'top',
+                                message: `Do you want to remove ${template?.name || 'this template'}?`,
+                                header: `Remove Template`,
+                                icon: 'pi pi-info-circle',
+                                acceptClassName: 'p-button-danger',
+                                accept: () => {
+                                    onDelete?.(template)
+                                }
+
+                            });
+                        }}
+                    />
+                    <Button
+                        className="h-[40px]"
+                        icon='pi pi-pencil'
+                        severity='info'
+                        label="Edit template"
+                        tooltipOptions={{ position: 'left' }}
+                        onClick={() => onClickEdit?.(template)}
+                    />
+                </div>
+            }
+            showActionBar
+        /> :
+        <EmptyPane icon={faPaperPlane} title='Select a template to show the graph' />
+}
+
+export default function Page() {
+
+    const [templates, setTemplates] = useState<IFlowBase[]>([]);
+    const [template, setTemplate] = useState<IFlow>();
+    const [editTemp, setEditTemp] = useState<IFlow>();
+    const { showMessage } = useLayoutContext();
+
+    const fetchTemplates = async () => {
+        const tmps = await getFlows('TEMPLATE');
+        setTemplates(tmps)
+    }
+
+    useEffect(() => {
+        fetchTemplates()
+    }, [])
+
+    return <div className="page-std">
+        <TitlePane title='Template' />
+        <Splitter className='shrink grow' style={{ height: '300px' }} layout='horizontal'>
+            <SplitterPanel className="px-[7px] " size={80}>
+                <TemplatePreviewer
+                    template={template}
+                    onClickEdit={async (tmp) => {
+                        setEditTemp(tmp)
+                    }}
+                    onDelete={async (tmp) => {
+                        const rsp = await deleteFlow(tmp?.id);
+                        if (rsp.data.status === 'failure') return;
+                        await fetchTemplates();
+                        setTemplate(pre => pre?.id === tmp.id ? undefined : pre)
+                    }}
+                />
+            </SplitterPanel>
+            <SplitterPanel className="overflow-auto px-[7px]" size={20}>
+                <FlowList
+                    defaultSelectedItem={template}
+                    flows={templates}
+                    onItemSelected={async (item) => {
+                        const tmp = await getFlow(item.id)
+                        setTemplate(tmp);
+                    }}
+                />
+
+            </SplitterPanel>
+        </Splitter>
+        <EditTemplateModal
+            editTemp={editTemp}
+            onOk={async (result) => {
+                const res = await updateFlow(result);
+                if (res.data.status === 'ok') {
+                    showMessage({
+                        type: 'success',
+                        message: res.data.message || 'Success',
+                    });
+                    setTemplate(pre => result.id === pre?.id ? result : pre)
+                    setEditTemp(undefined);
+                } else {
+                    showMessage({
+                        type: 'error',
+                        message: res.data.message || 'error',
+                    });
+                }
+            }}
+            onCancel={() => {
+                confirmDialog({
+                    position: 'top',
+                    message: `Are you sure you want to cancel without saving? You will lose every modification.`,
+                    header: `Cancel modify`,
+                    icon: 'pi pi-info-circle',
+                    acceptClassName: 'p-button-danger',
+                    accept: async () => {
+                        setEditTemp(undefined)
+                    },
+                });
+            }}
+        />
+    </div>
+
+}
+
+
