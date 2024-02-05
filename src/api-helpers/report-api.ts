2896c13db9505945e3ee19d36a18904d052c37f6
allen.lin <allen8673@gmail.com>
2024-02-05 08:31:46 +0800

    add new api to fetch  report list and deprecated old ones

--- src/api-helpers/report-api.ts
+++ src/api-helpers/report-api.ts
@@ -1,14 +1,10 @@
 import apiCaller from "./api-caller"
 
 import { ApiResult } from "@/interface/api"
-import { IJob, IJobStatus } from "@/interface/job"
-
-export const getJobs = async (workflowId?: string) => {
-    return (await apiCaller.get<ApiResult<IJob[]>>(`${process.env.NEXT_PUBLIC_REPORT}/getJobs/${workflowId || ''}/1`)).data
-}
+import { IJob, IJobList, IJobStatus } from "@/interface/job"
 
 export const runReport = async (formData: FormData) => {
-    return (await (apiCaller.post<ApiResult>(`${process.env.NEXT_PUBLIC_REPORT}/run`, formData, {
+    return (await (apiCaller.post<ApiResult<string>>(`${process.env.NEXT_PUBLIC_REPORT}/run`, formData, {
         headers: {
             "Content-Type": "multipart/form-data",
             // "x-rapidapi-host": "file-upload8.p.rapidapi.com",
@@ -25,10 +21,25 @@ export const checkJob = async (flowid: string) => {
     return (await apiCaller.get<ApiResult>(`${process.env.NEXT_PUBLIC_REPORT}/check/${flowid}`)).data
 }
 
-export const getJobsOngoing = async (flowid: string) => {
-    return (await apiCaller.get<ApiResult<IJob[]>>(`${process.env.NEXT_PUBLIC_REPORT}/getJobsOngoing/${flowid}`)).data.data
-}
 
 export const getJobItemStatus = async (jobid: string) => {
     return (await apiCaller.get<ApiResult<IJobStatus[]>>(`${process.env.NEXT_PUBLIC_REPORT}/getJobItemStatus/${jobid}`)).data.data
+}
+
+export const getJobslist = async (workflowId: string) => {
+    return (await apiCaller.get<ApiResult<IJobList>>(`${process.env.NEXT_PUBLIC_REPORT}/getJobslist/${workflowId}`)).data.data
+}
+
+/**
+ * @deprecated
+ */
+export const getJobs = async (workflowId?: string) => {
+    return (await apiCaller.get<ApiResult<IJob[]>>(`${process.env.NEXT_PUBLIC_REPORT}/getJobs/${workflowId || ''}/1`)).data
+}
+
+/**
+ * @deprecated
+ */
+export const getJobsOngoing = async (flowid: string) => {
+    return (await apiCaller.get<ApiResult<IJob[]>>(`${process.env.NEXT_PUBLIC_REPORT}/getJobsOngoing/${flowid}`)).data.data
 }
