cff1f29b385846cc0a0d1b8555bc5927e560ef8e
allen.lin <allen8673@gmail.com>
2024-02-21 16:55:23 +0800

    update table

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,48 +1,75 @@
 
-import { map, some } from 'lodash';
+import { map } from 'lodash';
+import { FilterMatchMode, FilterService } from 'primereact/api';
 import { Column as ColumnComp, } from 'primereact/column';
 import { DataTable, DataTableFilterMeta, } from 'primereact/datatable';
+import { useEffect, useState } from 'react';
 
 import ErrorBoundary from '../error-boundary';
 
-import { TableProps } from './table';
+import { Column, TableProps } from './table';
 
 export default function Table<T extends Record<string, any>>({ className, columns, value, key, ...tableProps }: TableProps<T>) {
     const table_key = key || 'table';
 
-    const filterMeta: DataTableFilterMeta | undefined =
-        some(columns, c => c.filter) ?
-            columns.reduce<DataTableFilterMeta>((result, column) => {
-                if (column.filter && column.key) {
-                    result[column.key as string] = { value: null, matchMode: column.filterMatchMode || 'contains' }
-                }
-                return result
-            }, {}) :
-            undefined;
+    const [filterMeta, setFilterMeta] = useState<DataTableFilterMeta | undefined>();
+    const [innerColumns, setInnerColumns] = useState<Column<T>[]>([]);
 
-    return <ErrorBoundary>
-        <DataTable
-            className={`zd-table ${className || ''}`}
-            key={table_key}
-            value={value}
-            selectionMode="single"
-            dataKey="id"
-            tableStyle={{ minWidth: '50rem' }}
-            filters={filterMeta}
-            {...tableProps}>
-            {map(columns, ({ key, header: title, body: format, ...c }, idx) => {
-                const col_key = (key as string) || idx
-                return (
-                    <ColumnComp
-                        key={`${table_key}-col-${col_key}`}
-                        field={key as string}
-                        header={title}
-                        body={format}
-                        className='ellipsis'
-                        {...c}
-                    />
-                )
-            })}
-        </DataTable >
-    </ErrorBoundary>
+    useEffect(() => {
+        let _filterMeta: DataTableFilterMeta | undefined;
+        const _columns: Column<T>[] = columns.reduce<Column<T>[]>((result, column) => {
+
+            let _column: Column<T> = column;
+
+            if (!column.key) {
+                result.push(_column);
+                return result;
+            }
+
+            // 如果有自定義filterFunction，則註冊到FilterService
+            // 並將filterFunction移除，改為使用CUSTOM模式
+            if (!!column.filterFunction) {
+                FilterService.register(`custom_${(column.key as string)}`, column.filterFunction);
+                _column = { ...column, filterMatchMode: FilterMatchMode.CUSTOM, filterFunction: undefined, showFilterMenuOptions: false };
+            }
+
+            // 如果有filter，則設定filterMeta
+            if (column.filter) {
+                _filterMeta = _filterMeta || {};
+                _filterMeta[column.key as string] = { value: null, matchMode: _column.filterMatchMode || 'contains' }
+            }
+
+            result.push(_column);
+            return result;
+        }, []);
+
+        setInnerColumns(_columns);
+        setFilterMeta(_filterMeta)
+    }, [columns])
+
+    return (
+        <ErrorBoundary>
+            <DataTable
+                className={`zd-table ${className || ''}`}
+                key={table_key}
+                value={value}
+                selectionMode="single"
+                dataKey="id"
+                tableStyle={{ minWidth: '50rem' }}
+                filters={filterMeta}
+                {...tableProps}>
+                {map(innerColumns, ({ key, ...c }, idx) => {
+                    const col_key = (key as string) || idx
+                    return (
+                        <ColumnComp
+                            key={`${table_key}-col-${col_key}`}
+                            field={key as string}
+                            className='ellipsis'
+                            {...c}
+                        />
+                    )
+                })}
+            </DataTable >
+        </ErrorBoundary>
+    )
 }
--- src/components/table/table.d.ts
+++ src/components/table/table.d.ts
@@ -1,13 +1,15 @@
 import { ColumnProps } from 'primereact/column';
 import { DataTablePropsSingle } from 'primereact/datatable';
 
-export interface Column<T extends Record<string, any>> extends Omit<ColumnProps, 'field' | 'key' | 'header' | 'body'> {
+export interface Column<T extends Record<string, any>> extends Omit<ColumnProps, 'field' | 'key' | 'header' | 'body' | 'filterFunction'> {
     // column key
     key?: keyof T;
     // column title
     header?: React.ReactNode;
     // 渲染column內容
     body?: (row: T) => JSX.Element | string | number;
+    // 自定義filter邏輯
+    filterFunction?: (value: any, filter: any) => boolean;
 }
 
 export interface TableProps<T extends Record<string, any>> extends Omit<DataTablePropsSingle<Array<T>>, 'value'> {
